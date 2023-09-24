4486444fa1e1011d2e2dc6b553aa1e45fb4c4e46
RobertoHaro <robertoharogonzalez@gmail.com>
2023-09-23 18:25:58 -0600

    function token filter while loop warning fixed

--- AnalizadorLexico/src/App/AL.java
+++ AnalizadorLexico/src/App/AL.java
@@ -118,7 +118,7 @@ public class AL extends javax.swing.JFrame {
         while(numberFlag || i==token.length()){ 
             switch(token.charAt(i)){
                 case '0', '1', '2', '3', '4', '5', '6', '7', '9' -> {}
-                case '.' -> numberType.concat(" Real"); type = "2";}
+                case '.' -> {numberType.concat(" Real"); type = "2";}
                 default -> {numberFlag = false;}
             }
             i++;
